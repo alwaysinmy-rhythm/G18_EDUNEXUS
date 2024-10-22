eea1fcda78513890b170487b15492e7c3de1bde8
Nishtha-8104 <patelnishtha78@gmail.com>
2024-10-22 20:12:59 +0530

    Uodated_viewProfile

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express= require("express");
-const authUser = require("../controller/authUser");
+const {authUser,viewProfile} = require("../controller/authUser");
 
 const router = express.Router();
 router.route('/login').post(authUser);
+router.route('/viewprofile').get(viewProfile);
+
  
 module.exports=router;  
--- server/controller/authUser.js
+++ server/controller/authUser.js
@@ -32,4 +32,60 @@ const authUser = async (req, res) => {
   }
 };
 
-module.exports = authUser;
+const viewProfile = async (req, res) => {
+  try {
+    console.log(req.body)
+    const studentSID = req.body.SID; 
+    const personalResult = await pool.query(`SELECT * FROM Student_Personal WHERE SID = $1`, [studentSID]);
+
+    if (personalResult.rows.length === 0) {
+      return res.status(404).json({ message: 'Student personal information not found!' });
+    }
+
+    const studentPersonal = personalResult.rows[0];
+
+    const academicResult = await pool.query(`SELECT * FROM Student_Academic WHERE SID = $1`, [studentSID]);
+
+    if (academicResult.rows.length === 0) {
+      return res.status(404).json({ message: 'Student academic information not found!' });
+    }
+
+    const studentAcademic = academicResult.rows[0];
+
+    const profile = {
+      personal_info: {
+        SID: studentPersonal.sid,
+        Sname: studentPersonal.sname,
+        Fname: studentPersonal.fname,
+        Mname: studentPersonal.mname,
+        Bdate: studentPersonal.bdate,
+        Addr_street: studentPersonal.addr_street,
+        Addr_city: studentPersonal.addr_city,
+        Addr_state: studentPersonal.addr_state,
+        Emergency_no: studentPersonal.emergency_no,
+        EmailId: studentPersonal.emailid,
+        gender: studentPersonal.gender,
+      },
+      academic_info: {
+        year: studentAcademic.year,
+        program: studentAcademic.program,
+        department: studentAcademic.department,
+        branch: studentAcademic.branch,
+        CPI: studentAcademic.cpi,
+        admission_rank: studentAcademic.admission_rank,
+        admission_through: studentAcademic.admission_through,
+      }
+    };
+
+    return res.status(200).json(profile);
+  } catch (error) {
+    console.error('Error during fetching profile:', error);
+    return res.status(500).json({ message: 'Internal server error occurred!!' });
+  }
+};
+
+module.exports = {
+  authUser,
+  viewProfile
+};
+
