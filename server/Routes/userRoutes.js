4229ddffdd17b3f86bcd88244c649eee26524753
FEFAR-SMIT <202201253@daiict.ac.in>
2024-11-14 14:32:10 +0530

    Adding Route for Course Notes

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -10,7 +10,7 @@ router.route('/login').post(authUser);
 router.route('/authRole').post(protect,authRole);
 router.route('/viewprofile').get(viewProfile);
 router.route('/editprofile').post(editProfile);
-
+router.route('/dashboard/mycourses/notes').get(courseNotes);
 
 
 
