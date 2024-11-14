28ceb2021bb6dcb027e8b6fbf7ad3b8a8bb64907
alwaysinmy-rhythm <rdhmpanchal@gmail.com>
2024-11-15 03:04:19 +0530

    integrated course registration at student side(minor remaining) Co-authored-by: 202201239 <202201239@daiict.ac.in>

--- server/Routes/userRoutes.js
+++ server/Routes/userRoutes.js
@@ -3,7 +3,7 @@ const {authUser,viewProfile,editProfile} = require("../controller/authUser");
 const {dashboard} = require("../controller/dashboard");
 const {mycourses} = require("../controller/mycourses");
 const {lab_assignment} = require("../controller/lab_assignment");
-
+const {getCourseRegistrationList,registerStudentPreferences} = require("../controller/course_registration");
 const router = express.Router();
 router.route('/login').post(authUser);
 router.route('/viewprofile').get(viewProfile);
@@ -15,6 +15,8 @@ router.route('/editprofile').post(editProfile);
 router.route('/dashboard').get(dashboard);
 router.route('/dashboard/mycourses').get(mycourses);
 router.route('/lab_assignment').get(lab_assignment);
+router.route('/course_registration').get(getCourseRegistrationList);
+router.route('/course_registration').post(registerStudentPreferences);
 
 
 
