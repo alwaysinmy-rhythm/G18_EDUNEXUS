b840226aef184df45fb3e468015706aac32e630d
SMIT FEFAR <202201253@daiict.ac.in>
2024-11-17 19:40:23 +0530

    Update courseNotes.js
    Due to integration with frontend changes are required

--- server/controller/courseNotes.js
+++ server/controller/courseNotes.js
@@ -1,25 +1,20 @@
 const pool = require("../config/db");
 
-// Takes CID and note_id in req body in json format.
+// Takes CID from the req params and sending back all the note links in JSON format.
 
 const courseNotes = async (req, res) => {
-    const { CID, note_id} = req.body;
-
-    // check that CID and note_id are in the body or not?
-    if (!CID || !note_id) {
-        return res.status(400).json({ error: 'Proper information is not provided!' });
-    }
+    const { CID } = req.params;
 
     try {
-        // Query to fetch the note_link
-        const { rows } = await pool.query('SELECT note_link FROM Notes WHERE CID = $1 AND note_id = $2', [CID, note_id]);
+        // Query to fetch the note_links
+        const { rows } = await pool.query('SELECT note_link FROM Notes WHERE CID = $1', [CID]);
 
         // If there are no entries then: 
         if (rows.length === 0) {
-            return res.status(404).json({ error: 'Note is not found for this particular course' });
+            return res.status(404).json({ error: 'Notes are not found for this particular course' });
         }
 
-        res.json({ note_link: rows[0].note_link });
+        res.json({ notes: rows });
     } catch (error) {
         console.error('Error during querying the database:', error);
         res.status(500).json({ error: 'Error in the server side!' });
