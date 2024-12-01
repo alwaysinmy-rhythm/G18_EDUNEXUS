582ffce3a0565c364c4d314b4d9d3401691b1738
SMIT FEFAR <202201253@daiict.ac.in>
2024-12-01 15:58:22 +0530

    Update courseNotes.js (#43)
    During Unit Testing, to cover all the edge cases, updating courseNotes.js

--- server/controller/courseNotes.js
+++ server/controller/courseNotes.js
@@ -3,22 +3,32 @@ const pool = require("../config/db");
 // Takes CID from the req params and sending back all the note links in JSON format.
 
 const courseNotes = async (req, res) => {
+
     const { CID } = req.params;
-
-    try {
-        // Query to fetch the note_links
-        const { rows } = await pool.query('SELECT note_link FROM Notes WHERE CID = $1', [CID]);
-
-        // If there are no entries then: 
-        if (rows.length === 0) {
-            return res.status(404).json({ error: 'Notes are not found for this particular course' });
-        }
-
-        res.json({ notes: rows });
-    } catch (error) {
-        console.error('Error during querying the database:', error);
-        res.status(500).json({ error: 'Error in the server side!' });
+    
+    // Validate CID
+    if (isNaN(CID) || Number(CID)<0 || !(Number.isInteger(Number(CID)))) {
+        console.log('Invalid CID branch triggered');
+        return res.status(400).json({ error: 'Invalid CID' });
     }
+    else{
+        console.log('valid CID branch triggered');
+        try {
+            // Query to fetch the note_links
+            const { rows } = await pool.query('SELECT note_link FROM Notes WHERE CID = $1', [CID]);
+    
+            // If there are no entries then: 
+            if (rows.length === 0) {
+                return res.status(404).json({ error: 'Notes are not found for this particular course' });
+            }
+    
+            return res.json({ notes: rows });
+        } catch (error) {
+            // console.error('Error during querying the database:', error);
+            return res.status(500).json({ error: 'Error in the server side!' });
+        }
+    }
+    
 };
 
 module.exports = {courseNotes}
